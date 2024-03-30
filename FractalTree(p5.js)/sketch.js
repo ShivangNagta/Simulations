db5b1750bbf54a4ec2b47570e1b8901d97b8ef4f
Shivang Nagta <141389245+ShivangNagta@users.noreply.github.com>
2024-03-30 10:52:37 +0530

    Add files via upload

---
+++ FractalTree(p5.js)/sketch.js
@@ -0,0 +1,35 @@
+
+
+function setup() {
+  var angle = PI / 4;
+  createCanvas(400, 400);
+  slider = createSlider(0, TWO_PI, PI / 4)
+}
+
+function draw() {
+  background(50);
+  angle = slider.value();
+  stroke(255);
+  translate(200, height)
+  branch(100);
+}
+
+
+function branch(len){
+  line(0, 0, 0, -len)
+  translate(0, -len)
+  if (len > 4)
+    {
+      push();
+      rotate(angle)
+      branch(len*0.67)
+      pop()
+      push()
+      rotate(-angle);
+      branch(len*0.67);
+      pop();
+    }
+  }
+  
+  
+  
